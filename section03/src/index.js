e4d8e459f9a6271feb1f665e8ff90f928595c063
Jinacker <rlawls1448@naver.com>
2025-02-24 11:30:21 +0900

    Node.js 라이브러리 사용

--- section03/src/index.js
+++ section03/src/index.js
@@ -2,7 +2,7 @@
 // 여기서 moduleDate 는 객체 배열임.
 
 // ES module 사용법. 주의점: .js도 꼭 넣기 위와 다름.
-import mul, {add, sub} from "./math.js"; // import 써서 훨씬 간단
+// import mul, {add, sub} from "./math.js"; // import 써서 훨씬 간단
 // default인 애는 이렇게 따로 import 해줘야함.
 
 
@@ -16,5 +16,15 @@ console.log();
 
 //const {add, sub} = require("./math"); // 간편
 
-console.log(add(1,2)); // 이러면 그냥 바로 부르기 가능.
-console.log(sub(1,2));
+//console.log(add(1,2)); // 이러면 그냥 바로 부르기 가능.
+//console.log(sub(1,2));
+
+
+////// 라이브러리 실습 
+import randomColor from "randomcolor"; // 이름만 명시해주면됨
+
+const color = randomColor();
+console.log(color);
+
+
+
