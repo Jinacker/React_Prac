e8364295c8b06561e43dc7f7348e325a7450c93c
Jinacker <rlawls1448@naver.com>
2025-02-25 16:42:57 +0900

    State와 Props (심화)

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,16 +1,17 @@
 import './App.css';
 import {useState} from "react";
 
-function App() { 
-  const [count, setCount]= useState(0); // 리턴값이 state(현재값)과 setState(변화시키는값)이다.
-  const [light, setLight] = useState("OFF");// 전구 껐다키기 state
-  // 여기서 의문을 가질수있음 => 그냥 변수로 쓰면 안되나? 꼭 State 써야되나?
-  // 리렌더링 될라면 State 써야됨 다른애 쓰면 가변적이지 않아서 바로바로 안바뀜.
+const Bulb = () => // 전구 컴포넌트 따로 만들기
+{
+  const [light, setLight] = useState("OFF");
 
-  return (
-  <>
+  console.log(light);
+  return ( // 시발 return 옆에 꼭 ( 붙이기) => 엔터해서 줄바꾸면 작동안함.
     <div>
-      <h1>{light}</h1>
+      {light === "ON" ? 
+      (<h1 style = {{backgroundColor: "orange"}}>ON</h1>) 
+      : (<h1 style = {{backgroundColor: "gray"}}>OFF</h1>)}
+
       <button
         onClick = {() => {
           setLight(light === "OFF" ? "ON" : "OFF"); // 이걸로 껐따 킬수있게 상태 전달.
@@ -18,13 +19,15 @@ function App() {
       > 
       {(light === "ON" ? "OFF" : "ON")}
       </button>
-
-
-
     </div>
+  );
+}
 
-    <div>
-    
+const Counter = () => {
+  const [count, setCount]= useState(0);
+  
+  return(
+  <div>
       <h1>{count}</h1>
       <button 
         onClick={() => {
@@ -33,7 +36,34 @@ function App() {
     >
       +
       </button>
+      <button
+      onClick={() => {
+        setCount(count-1);
+      }}
+      >
+      -
+      </button>
     </div>
+  );
+}
+
+
+
+// 리랜더링이 일어나는 3가지 기준
+// 1. state 변경시
+// 2. props 변경시
+// 3. 부모 컴포넌트 변경시 => 자식도 변경
+// 이래서... => 컴포넌트는 최대한 분리해놓는게 좋다.
+// 안그러면 + 버튼 눌렀는데 전구에까지 영향가서 리렌더됨.
+// 더 나아가서 그냥 아예 다른 파일로 분리를 해놓는게 좋다. 
+// 메인인 App은 최대한 간결할 수 있게 ㅇㅇ
+
+
+function App() { 
+  return (
+  <>
+    <Bulb/>
+    <Counter/>
   </>
   );
 }
