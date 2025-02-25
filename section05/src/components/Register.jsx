fecea0514b11a65e0367f6fd7c4e5fce13effde6
Jinacker <rlawls1448@naver.com>
2025-02-25 21:26:37 +0900

    useRef

--- section05/src/components/Register.jsx
+++ section05/src/components/Register.jsx
@@ -5,7 +5,7 @@
 // 4. 자기소개 
 
 import {useState} from "react";
-
+import {useRef} from "react"; // useRef 실습 ! => 얘도 리엑트에서 제공해주는 내장함수임.
 
 const Register = () => {
 
@@ -17,14 +17,29 @@ const Register = () => {
         bio: "",
     });
 
+    const countRef = useRef(0);
+    const inputRef = useRef();
+    console.log("Register 렌더링");
+    // let count = 1; => 얘로 쓰면 결국 값 안바뀜 => 계속 리렌더링 되면서 전체가 실행, 다시 1로 리셋됨.
+    // 그래서 useRef 쓰는거임 => 얘는 기능적으로 리셋 안되게 되어있음.
 
     // 통합 이벤트 핸들러
     const onChange = (e) => {
+        countRef.current++; // 기능 => 수정횟수가 올라감
+        console.log(countRef.current); // 수정횟수 출력.
         console.log(e.target.name, e.target.value);
         setInput({ // 객체를 만들어서 전달 
             ...input,
             [e.target.name]: e.target.value, // [ (name)프로퍼티 키 자리 ]: 벨류 => 이런식으로 작동함. 
         }) // 개똑똑하네....
+    }
+
+    const onSubmit = () =>{
+        if (input.name ===""){
+            // 이름을 입력하는 DOM 요소에 포커스 
+            inputRef.current.focus();
+
+        }
     }
 
     console.log(input);
@@ -34,8 +49,9 @@ const Register = () => {
     return ( // 이벤트 핸들러와 state 생성 => onChange 쓸때 초기값 있으면 value = {}도 꼭 써주기.
         <div>
             <div>
-                
-                <input name = "name" // e.target.name 에 name으로 들어가라고 이렇게 설정.
+                <input 
+                ref = {inputRef}
+                name = "name" // e.target.name 에 name으로 들어가라고 이렇게 설정.
                 value = {input.name} 
                 onChange= {onChange} 
                 placeholder="이름"></input>
@@ -43,7 +59,6 @@ const Register = () => {
             </div>
 
             <div>
-                
                 <input name = "birth" value = {input.birth} onChange = {onChange} type = "date"></input>
                 {input.birth}
             </div>
@@ -63,6 +78,8 @@ const Register = () => {
                 </textarea>
                 {input.bio}
             </div>
+
+            <button onClick = {onSubmit}>제출</button>
         </div> 
     );
 }
