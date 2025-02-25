a5b76a575808424a7021eb2e29b8f5cd9d255a18
Jinacker <rlawls1448@naver.com>
2025-02-25 22:08:54 +0900

    카운터앱 UI 만들기

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,25 +1,12 @@
 import './App.css';
-import Bulb from "./components/Bulb.jsx";
-import Counter from "./components/Counter.jsx";
-
-
-// 리랜더링이 일어나는 3가지 기준
-// 1. state 변경시
-// 2. props 변경시
-// 3. 부모 컴포넌트 변경시 => 자식도 변경
-// 이래서... => 컴포넌트는 최대한 분리해놓는게 좋다.
-// 안그러면 + 버튼 눌렀는데 전구에까지 영향가서 리렌더됨.
-// 더 나아가서 그냥 아예 다른 파일로 분리를 해놓는게 좋다. 
-// 메인인 App은 최대한 간결할 수 있게 ㅇㅇ
-
+import HookExam from "./components/HookExam.jsx";
 
 function App() { 
   return (
   <>
-    <Bulb/>
-    <Counter/>
+    <HookExam/>
   </>
   );
-}
+} 
 
 export default App;
---
+++ section06-counter_app/src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css';
+import Viewer from "./components/Viewer.jsx";
+import Controller from './components/Controller.jsx';
+
+function App() {
+  return (
+    <div className = "App">
+    <h1>Simple Counter</h1>
+
+    <section>
+       <Viewer></Viewer>
+    </section>
+
+    <section>
+       <Controller></Controller>
+    </section>
+      
+    </div>
+  )
+}
+
+export default App
