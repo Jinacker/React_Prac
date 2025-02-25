fa21068cef33b5e1e360baf91fccad104404b717
Jinacker <rlawls1448@naver.com>
2025-02-25 15:21:26 +0900

    컴포넌트 선언

---
+++ section05/src/components/Header.jsx
@@ -0,0 +1,10 @@
+function Header() // 컴포넌트 맨 첫자는 꼭!!!!! 대문자 => 아니면 컴포넌트로 인정안됨.
+{ // 내가 만든 header 함수 컴포넌트
+  return (
+    <header>
+      <h1>header</h1>
+    </header>
+  );
+}
+
+export default Header; // Es 모듈로 추출.
