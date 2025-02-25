57d09b55a4efb0e607b48e05ff764f7723af380b
Jinacker <rlawls1448@naver.com>
2025-02-25 19:35:29 +0900

    통합 state와 이벤트 핸들러로 회원가입 깔끔 개선

--- section05/src/components/Register.jsx
+++ section05/src/components/Register.jsx
@@ -2,65 +2,66 @@
 // 1. 이름
 // 2. 생년월일
 // 3. 국적
-// 4. 자기소개
+// 4. 자기소개 
+
 import {useState} from "react";
 
 
 const Register = () => {
 
-    // state 설정
-    const [name, setName] = useState(""); // 초기값 "이름"인 state 생성
-    const [birth, setBirth] = useState("");
-    const [country, setCountry] = useState("");
-    const [bio, setBio] = useState("");
+    // 통합 state 설정 => 이렇게 그냥 객체로 만들어서 때려박기.
+    const [input, setInput] = useState({
+        name: "",
+        birth: "",
+        country: "",
+        bio: "",
+    });
 
 
-    // 이벤트 핸들러들
-    const onChangeBirth = (e) => {
-        setBirth(e.target.value);
+    // 통합 이벤트 핸들러
+    const onChange = (e) => {
+        console.log(e.target.name, e.target.value);
+        setInput({ // 객체를 만들어서 전달 
+            ...input,
+            [e.target.name]: e.target.value, // [ (name)프로퍼티 키 자리 ]: 벨류 => 이런식으로 작동함. 
+        }) // 개똑똑하네....
     }
 
-    const onChangeName = (e) => {
-        setName(e.target.value); // 이렇게 input에 들어간 값에 접근가능하다.
-    };
-
-    const onChangeCountry = (e) => {
-        setCountry(e.target.value);
-    }
-
-    const onChangeBio = (e) => {
-        setBio(e.target.value);
-    }
-
+    console.log(input);
 
     // 메인문
 
     return ( // 이벤트 핸들러와 state 생성 => onChange 쓸때 초기값 있으면 value = {}도 꼭 써주기.
         <div>
             <div>
-                <input value = {name} onChange= {onChangeName} placeholder="이름"></input>
-                {name}
+                
+                <input name = "name" // e.target.name 에 name으로 들어가라고 이렇게 설정.
+                value = {input.name} 
+                onChange= {onChange} 
+                placeholder="이름"></input>
+                {input.name}
             </div>
 
             <div>
-                <input value = {birth} onChange = {onChangeBirth} type = "date"></input>
-                {birth}
+                
+                <input name = "birth" value = {input.birth} onChange = {onChange} type = "date"></input>
+                {input.birth}
             </div>
 
             <div>
-                <select value = {country} onChange = {onChangeCountry}>
+                <select name = "country" value = {input.country} onChange = {onChange}>
                     <option></option>
                     <option value ="kr">한국</option>
                     <option value = "uk">영국</option>
                     <option value = "us">미국</option>
                 </select>
-                {country}
+                {input.country}
             </div>
 
-            <div>
-                <textarea value = {bio} onChange = {onChangeBio}>
+            <div>  
+                <textarea name = "bio" value = {input.bio} onChange = {onChange}>
                 </textarea>
-                {bio}
+                {input.bio}
             </div>
         </div> 
     );
