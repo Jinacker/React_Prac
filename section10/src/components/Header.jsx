9d64087cd514d677a1c89707b705f019b37c8628
Jinacker <rlawls1448@naver.com>
2025-02-27 18:09:12 +0900

    memo를 이용한 컴포넌트 렌더링 최적화

--- section10/src/components/Header.jsx
+++ section10/src/components/Header.jsx
@@ -1,4 +1,5 @@
 import "./Header.css";
+import { memo } from "react"; // memo => 얘도 훅인데... => use가 아니네?
 
 const Header = () => {
     return (
@@ -9,4 +10,12 @@ const Header = () => {
     )
 };
 
-export default Header; 
+// // memo(컴포넌트) 해서 => 추출을 memo된 걸로함.
+// const memorizedHeader = memo(Header);
+
+// // 일케 기존 Header가 아니라 바뀐 Header export
+// export default memorizedHeader; 
+
+// 아니면 걍 이렇게 한줄로 ㄱㄱ
+
+export default memo(Header);
--- section10/src/components/TodoItem.jsx
+++ section10/src/components/TodoItem.jsx
@@ -1,5 +1,8 @@
 import "./TodoItem.css";
+import {memo} from "react";
 
+// 기본적으로 값 같다고 같은게 아니라 주소값을 기반으로 비교
+// 얘네도 결국 맨날 생성될때마다 다른 주소값으로 설정된겨 => 그래서 결국 바꼈기때문에 memo해도 계속 리렌더링된거.
 const TodoItem = ({id, isDone,content, date, onUpdate, onDelete}) => {
 
     const onChangeCheckbox = () => {onUpdate(id)}; // 체크박스용 이벤트 헨들러
@@ -18,4 +21,19 @@ const TodoItem = ({id, isDone,content, date, onUpdate, onDelete}) => {
     </div>);
 };
 
-export default TodoItem;
+// 얘도 안에 내용이 바뀔때만 렌더링되게 memo
+// 고차 컴포넌트 (HOC)
+export default memo(TodoItem, (prevProps, nextProps) => { // 과거Props와 현재Props가 있다
+    // 반환값에 따라, props가 바뀌엇는지 안바뀌었는지 판단
+    // T => props 안바뀜 => 리렌더링 x
+    // F => props 바뀜 => 리렌더링 o
+    if(prevProps.id !== nextProps.id) return false; // 이런식으로 직접 비교해서 리렌더링 제어.
+    if(prevProps.isDone !== nextProps.isDone) return false; 
+    if(prevProps.content !== nextProps.content) return false;
+    if(prevProps.Date !== nextProps.Date) return false;
+
+    return true;
+}); // 이렇게 콜백함수 넣어버리면 => 이런식으로 직접 비교해서 가능.
+
+// memo => 과거의 props와 현재 props와 비교해서 실행할지 생각함. => 얕은 비교함 
+// => 객체 타입의 값은 항상 다른 주소값 => memo에서 props 바꼈다고 판단.
